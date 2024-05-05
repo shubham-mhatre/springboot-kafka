f9e6fe46fa47afbc81b0bf35d45b11dcab52aa35
shubham <shubhammahtre@bdo.in>
2024-05-05 15:20:42 +0530

    initial commit

---
+++ Ticket-Booking-User/src/main/java/com/sm/cl/TicketBookingClientApplication.java
@@ -0,0 +1,13 @@
+package com.sm.cl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingClientApplication.class, args);
+	}
+
+}
---
+++ Ticket-booking-server/src/main/java/com/sm/sv/TicketBookingServerApplication.java
@@ -0,0 +1,13 @@
+package com.sm.sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingServerApplication.class, args);
+	}
+
+}
